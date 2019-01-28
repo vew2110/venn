26dd7bad146414b021d9c768b136d6b491e6c517
Victoria Weller-von Ahlefeld <vew2110@columbia.edu>
2019-01-27 22:01:03 -0500

    table names initiated

--- app.py
+++ app.py
@@ -1,13 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import yaml
+import pymysql.cursors
+
 app = Flask(__name__)
+
+def unlock():
+     with open("secret.yml", 'r') as stream:
+         return yaml.load(stream)
+
+# MySQL configurations
+config = unlock()
+
+con = pymysql.connect(
+    host=config['dbhost'],
+    user=config['user'],
+    password=config['password'],
+    db=config['dbname'],
+    cursorclass=pymysql.cursors.DictCursor
+)
+
+def connect():
+    config = unlock()
+    app.config
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    with con.cursor() as cursor:
+        cursor.execute('select * from users')
+        result = cursor.fetchall()
+    return jsonify(result)
 
-@app.route('/new', methods=['POST', 'GET'])
-def form(name=None):
+@app.route('/users/new', methods=['POST', 'GET'])
+def form():
     if request.method == 'GET':
-        return render_template('form.html', name=name)
+        return render_template('form.html')
     elif request.method == 'POST':
+        name = request.form['name']
+        with con.cursor() as cursor:
+            sql='INSERT INTO users (name) VALUES (%s)'
+            cursor.execute(sql, (name,))
+        con.commit()
         return 'Thx'
